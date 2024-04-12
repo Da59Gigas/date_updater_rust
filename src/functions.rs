c232dbb3b5f9c1ae7e2476624c38bb58aaffd760
Da59Gigas <da59gigas@gmail.com>
2024-04-12 16:38:24 +0100

    Finished creating the check_connection function in functions.rs.

--- src/functions.rs
+++ src/functions.rs
@@ -4,12 +4,12 @@ pub fn check_connection(__target: Option<&str>, __count: Option<u8>, __timeout: 
     let target = __target.unwrap_or("1.1.1.1");
     let count = __count.unwrap_or(1);
     let timeout = __timeout.unwrap_or(6);
-    // python: command = f"ping -c {count} -t {timeout} {target}"
-    // try to figure out how to either make the equivalent of an fstring
-    // or convert u8 to String or &str
-    let mut output = Command::new("ping").arg("-c").arg(count)
-        .arg("-t").arg(timeout).arg(target);
-    let nothing = output.output().expect("Command failed to execute.");
+    let count_str = format!("{}", count);
+    let timeout_str = format!("{}", timeout);
+
+    let mut output2 = Command::new("ping");
+    output2.arg("-c").arg(count_str).arg("-t").arg(timeout_str).arg(target);
+    let nothing = output2.output().expect("Command failed to execute.");
     return match nothing.status.success() {
         true => { true }
         false => { false }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod functions;
 
-use pythonish::cmd;
+//use pythonish::cmd;
 use functions::check_connection;
 
 fn main() {
-    let _result = cmd(String::from("ping -c 1 -t 6 1.1.1.1"));
-    let _result2 = check_connection(None, None, None);
-    print!("{}", _result)
+    println!("Running test:");
+    let _result = check_connection(None, None, None);
+    println!("{}", _result);
 }
